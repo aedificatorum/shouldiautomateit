b8ba4c9f5c586ebd089b8665a9591c158cad4d8e
celinechole <github@celinechole.com>
2019-04-22 15:22:48 -0400

    add input table

--- src/App.js
+++ src/App.js
@@ -1,17 +1,30 @@
-import React, { Component } from 'react';
-import './App.css';
-
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
- 
   render() {
     return (
-      <div className="App">
-     <h1>Should I automate</h1>
-     <div className="container"></div>
-      </div>
-    
+      <div>
+        <h1>Should I automate</h1>
 
+        <div className="row">
+          <form className="form">
+            <label for="automate-time">Time to automate</label>
+            <input type="text" id="automate-time" placeholder="Time in minutes" />
+            <label for="complete-time">Time to complete</label>
+            <input type="text" id="complete-time" placeholder="Time in minutes" />
+            <label for="times-month">Times per month</label>
+            <input type="text" id="times-month" placeholder="Time in minutes" />
+          </form>
+          <table>
+            <tr>
+              <th>test</th>
+            </tr>
+          </table>
+          {/* <div className="row">
+        </div> */}
+      </div>
+      </div>
     );
   }
 }
