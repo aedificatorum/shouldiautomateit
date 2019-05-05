4e189f5a4168eb8a785206a5035c2aaea64951f5
celinechole <github@celinechole.com>
2019-05-05 12:00:27 -0400

    added calculations for roi slow

--- src/Calculator.js
+++ src/Calculator.js
@@ -1,19 +1,22 @@
-function Calculator(duration, automationTime, numberOfTimesPerMonth, numberOfMonths = 36) {
+function Calculator(duration, automationTime, numberOfTimesPerMonth, numberOfMonths = 36, maxSpeedUp = 0.5, maxSlowDown = 3) {
     let rows = [];
 
     for(let i = 0; i < numberOfMonths; i++) {
         let month = i + 1;
         let timeSaved = duration * numberOfTimesPerMonth * month;
+        let maxAutomateTime = automationTime * maxSlowDown;
+
         rows.push({
             month,
             timeSaved,
-            roiSlow: timeSaved - automationTime,
+            roiSlow: timeSaved - maxAutomateTime,
             roiMed: timeSaved - automationTime,
             roiFast: timeSaved - automationTime,
-            shouldIAutomate: "No"
+            shouldIAutomate: "no"
           }
         );
     }
+
 
     return rows;
 }
