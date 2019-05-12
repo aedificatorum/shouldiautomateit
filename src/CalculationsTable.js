cacb7c496df0b17e62d97cc8985846d59ec92a5f
celinechole <github@celinechole.com>
2019-05-11 20:38:08 -0400

    add color

--- src/CalculationsTable.js
+++ src/CalculationsTable.js
@@ -23,7 +23,7 @@ const CalculationsTable = ({ table }) => {
                 <td>{row.roiSlow}</td>
                 <td>{row.roiMed}</td>
                 <td>{row.roiFast}</td>
-                <td>{row.shouldIAutomate}</td>
+                <td className={row.shouldIAutomate.toLowerCase()}>{row.shouldIAutomate}</td>
               </tr>
             )
           })}
