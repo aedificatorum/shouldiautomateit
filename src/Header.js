891054709f35e9c3c8372f03048caf744e99499f
celinechole <github@celinechole.com>
2019-05-12 14:32:53 -0400

    migrate header component

--- src/Header.js
+++ src/Header.js
@@ -1,9 +1,33 @@
-import React from "react"
+import React from "react";
 
 const Header = () => {
   return (
-
-  )
-}
+    <header>
+      <nav>
+        <div class="nav-wrapper black">
+          <a href="#!" class="navbar-title">
+            Should I automate it?
+          </a>
+          <a href="/" data-target="mobile-nav" class="sidenav-trigger">
+            <i class="fas fa-bars" />
+          </a>
+          <ul class="right hide-on-med-and-down">
+            <li>
+              <a href="#about">About</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <ul class="sidenav" id="mobile-nav">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+      </ul>
+    </header>
+  );
+};
 
 export default Header;
