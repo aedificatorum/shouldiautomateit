c671c94af2626519088241aa21e47e4749930d92
Tim Addison <git@tjaddison.com>
2019-05-12 15:05:31 -0400

    homepage path set

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a6c949454b75005e4d03bbce9268f87d.js"
+  "/shouldIAutomateIt/precache-manifest.6e81bbbf632836dfd9ef4c0297f1bc3f.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/shouldIAutomateIt/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
