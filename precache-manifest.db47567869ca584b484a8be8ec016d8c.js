a0d7f106a486531b4b07aa25962a26d00753a9b2
Tim Addison <git@tjaddison.com>
2019-05-12 15:54:20 -0400

    adding 1% more awesome

---
+++ precache-manifest.db47567869ca584b484a8be8ec016d8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3765f8268df097f78f39",
+    "url": "/shouldiautomateit/static/js/runtime~main.e655c175.js"
+  },
+  {
+    "revision": "553a0d69d0957ab9b132",
+    "url": "/shouldiautomateit/static/js/main.1f3810f2.chunk.js"
+  },
+  {
+    "revision": "29591bf350dfb73bb3a4",
+    "url": "/shouldiautomateit/static/js/2.2d69c649.chunk.js"
+  },
+  {
+    "revision": "553a0d69d0957ab9b132",
+    "url": "/shouldiautomateit/static/css/main.ea7947a4.chunk.css"
+  },
+  {
+    "revision": "29591bf350dfb73bb3a4",
+    "url": "/shouldiautomateit/static/css/2.475ec6e2.chunk.css"
+  },
+  {
+    "revision": "1987d1538c92eb152023af6047b56339",
+    "url": "/shouldiautomateit/index.html"
+  }
+];
