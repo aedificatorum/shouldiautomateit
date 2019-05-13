705d35b544a96930bcae73b11a40ac2951738d0f
celinechole <github@celinechole.com>
2019-05-13 09:55:20 -0400

    implement nav link

--- src/Header.js
+++ src/Header.js
@@ -1,32 +1,42 @@
 import React from "react";
+import { NavLink, HashRouter, Route, Switch } from "react-router-dom";
+import Home from "./Home";
+import About from "./About";
 
 const Header = () => {
   return (
-    <header>
+    <React.Fragment>
       <nav>
         <div className="nav-wrapper black">
-          <a href="#!" className="navbar-title">
+          <NavLink to="/" className="navbar-title">
             Should I automate it?
-          </a>
+          </NavLink>
           <a href="/" data-target="mobile-nav" className="sidenav-trigger">
             <i className="fas fa-bars" />
           </a>
           <ul className="right hide-on-med-and-down">
             <li>
-              <a href="#about">About</a>
+              <NavLink to="/about">About</NavLink>
             </li>
           </ul>
         </div>
       </nav>
+
       <ul className="sidenav" id="mobile-nav">
         <li>
           <a href="/">Home</a>
         </li>
         <li>
-          <a href="#about">About</a>
+          <NavLink to="/about">About</NavLink>
         </li>
       </ul>
-    </header>
+
+      <Switch>
+        <Route path="/about" component={About} />
+        <Route exact path="/" component={Home} />
+        {/* <Route path="*" component={NotFound} /> */}
+      </Switch>
+    </React.Fragment>
   );
 };
 
