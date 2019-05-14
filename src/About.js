afc0684b406c1fba240d5329ad8ddb603423a241
celinechole <github@celinechole.com>
2019-05-14 08:57:01 -0400

    add style

--- src/About.js
+++ src/About.js
@@ -3,27 +3,43 @@ import React from "react";
 const About = () => {
   return (
     <div className="container">
-      <h3>About</h3>
-      <div className="about-page">
-        <p>
-          <i>Should I automate it?</i> is designed for anyone interested to see
-          when a manual process would benefit from being automated.
-        </p>
-        <p>
-          It was inspired by{" "}
-          <a href="https://xkcd.com/1205/" target="blank">
-            xkcd 1205
-          </a>
-          .
-        </p>
-        <p>
-          <a
-            href="https://github.com/aedificatorum/shouldiautomateit"
-            target="blank"
-          >
-            GitHub repository
-          </a>
-        </p>
+      <div className="about-container">
+        <h3>About</h3>
+        <div className="about-page">
+          <p>
+            <b>
+              <i>Should I automate it?</i>
+            </b>{" "}
+            is designed for anyone interested to see when a manual process would
+            benefit from being automated.
+          </p>
+          <h5 className="about-title">Inspiration</h5>
+          <p>
+            It was inspired by{" "}
+            <a href="https://xkcd.com/1205/" target="blank">
+              xkcd 1205
+            </a>
+            .
+          </p>
+          <h5 className="about-title">GitHub repository</h5>
+          <p>
+            <a
+              href="https://github.com/aedificatorum/shouldiautomateit"
+              target="blank"
+            >
+              Should I automate it?
+            </a>
+          </p>
+          <h5 className="about-title">Built by</h5>
+          <p>
+            <a
+              href="https://github.com/aedificatorum"
+              target="blank"
+            >
+              Aedificatorum
+            </a>
+          </p>
+        </div>
       </div>
     </div>
   );
