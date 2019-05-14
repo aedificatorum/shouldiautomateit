35f1c9642902b09e4686fbd561eb4ffc5e3794b0
celinechole <github@celinechole.com>
2019-05-14 09:02:37 -0400

    add skeleton

--- src/Help.js
+++ src/Help.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
 const Help = () => {
   return (
-    <div>
-      Help
+    <div className="container">
+      <h3>How to use Should I automate it?</h3>
+      <h5>Basic Options</h5>
+      <h5>Advanced Options</h5>
     </div>
-  )
-}
+  );
+};
 
-export default Help
+export default Help;
