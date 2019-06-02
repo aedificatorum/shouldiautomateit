6e46376c0f5f093fadaf7e2c7bd533f6b8e5afd9
Tim Addison <git@tjaddison.com>
2019-06-02 16:26:01 -0400

    Add base appinsights tracking

--- src/index.js
+++ src/index.js
@@ -3,6 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApplicationInsights } from '@microsoft/applicationinsights-web';
+import { ReactPlugin } from '@microsoft/applicationinsights-react-js';
+import { createBrowserHistory } from "history";
+ 
+const browserHistory = createBrowserHistory({ basename: '' });
+var reactPlugin = new ReactPlugin();
+var appInsights = new ApplicationInsights({
+    config: {
+        instrumentationKey: '9661dd41-959a-4da4-9c6b-1b9169c3d5bf'
+    },
+    extensions: [reactPlugin],
+    extensionConfig: {
+      [reactPlugin.identifier]: { history: browserHistory }
+    }
+});
+appInsights.loadAppInsights();
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
