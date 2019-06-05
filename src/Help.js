2c54773322a9cd6fa5e5e24a8da9a81f65d7c7f5
Tim Addison <git@tjaddison.com>
2019-06-04 20:47:44 -0400

    Notes for help

--- src/Help.js
+++ src/Help.js
@@ -5,7 +5,18 @@ const Help = () => {
     <div className="container">
       <h3>How to use Should I automate it?</h3>
       <h5>Basic Options</h5>
+      <ul>
+        <li>How often</li>
+        <li>With what frequency</li>
+        <li>How much time could be saved</li>
+        <li>How long would it take to automate</li>
+      </ul>
       <h5>Advanced Options</h5>
+      <ul>
+        <li>How many months to forecast</li>
+        <li>How much faster might the automation be</li>
+        <li>How much slower might the automation be</li>
+      </ul>
     </div>
   );
 };
