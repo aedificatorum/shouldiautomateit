ba8a60c35a5f55baa0a8344d5ec924880cace3e6
celinechole <github@celinechole.com>
2019-06-08 16:53:27 -0400

    create summary table

--- src/Summary.js
+++ src/Summary.js
@@ -1,19 +1,37 @@
-import React from 'react';
+import React from "react";
 
 const Summary = ({ calculationsTable }) => {
- 
-  console.log(calculationsTable[1])
+  console.log(calculationsTable[1]);
   return (
- <div className="row">
-   <p>Summary</p>
-   <p>{calculationsTable[2] ? ('3 months: ' + calculationsTable[2].shouldIAutomate ) : ''}</p>
-   <p>{calculationsTable[5] ? ('6 months: ' + calculationsTable[5].shouldIAutomate ) : ''}</p>
-   <p>{calculationsTable[11] ? ('12 months: ' + calculationsTable[11].shouldIAutomate ) : ''}</p>
-   <p>{calculationsTable[23] ? ('24 months: ' + calculationsTable[23].shouldIAutomate ) : ''}</p>
-   <p>{calculationsTable[35] ? ('36 months: ' + calculationsTable[35].shouldIAutomate ): ''}</p>
- </div>
-  )
-  
-}
+    <div className="row">
+      <p>Summary</p>
+      <p>
+        {calculationsTable[2]
+          ? "3 months: " + calculationsTable[2].shouldIAutomate
+          : ""}
+      </p>
+      <p>
+        {calculationsTable[5]
+          ? "6 months: " + calculationsTable[5].shouldIAutomate
+          : ""}
+      </p>
+      <p>
+        {calculationsTable[11]
+          ? "12 months: " + calculationsTable[11].shouldIAutomate
+          : ""}
+      </p>
+      <p>
+        {calculationsTable[23]
+          ? "24 months: " + calculationsTable[23].shouldIAutomate
+          : ""}
+      </p>
+      <p>
+        {calculationsTable[35]
+          ? "36 months: " + calculationsTable[35].shouldIAutomate
+          : ""}
+      </p>
+    </div>
+  );
+};
 
 export default Summary;
