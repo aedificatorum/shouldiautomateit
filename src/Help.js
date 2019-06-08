93adafc04e69993f578c93fbd01f11edfd64798d
Tim Addison <git@tjaddison.com>
2019-06-08 15:39:06 -0400

    Add help detail

--- src/Help.js
+++ src/Help.js
@@ -7,19 +7,33 @@ const Help = () => {
   return (
     <div className="container">
       <h3>How to use Should I automate it?</h3>
+      <p>
+        The tool is designed to help evaluate whether or not it is worth investing time into
+        automating (in whole or in part) a task.
+      </p>
+      <p>
+        By using the inputs to determine how often the task is completed, how much time could be saved, and 
+        how much time it would take to automate - the tool shows a table of the Return on Investiment (ROI).  
+        This is computed as time saved (times the task is completed * time saved per task) less time to automate.  
+        A positive value indicates that in that month the automation has 'paid off'.  A negative value indicates 
+        that more time was spent automating than was saved.  The additional ROI columns represent the under or over 
+        estimation cases.
+      </p>
       <h5>Basic Options</h5>
-      <ul>
-        <li>How often</li>
-        <li>With what frequency</li>
-        <li>How much time could be saved</li>
-        <li>How long would it take to automate</li>
-      </ul>
+      <p>
+        Fill out the form so that reading the sentence correctly describes how often you do the
+        task (e.g. 3 times every weekday), and the amount of time you could save <strong>each time </strong>
+        is noted.  The final input is how long it would take to complete the automation.
+      </p>
       <h5>Advanced Options</h5>
-      <ul>
-        <li>How many months to forecast</li>
-        <li>How much faster might the automation be</li>
-        <li>How much slower might the automation be</li>
-      </ul>
+      <p>
+        By default the tool assumes that the estimate might be potentially twice as long (0.5 - 50%) as
+        it will actually take.  It also assumes that it may be underestimating by up to three times (3.0 - 300%).
+        If you are more (or less) certain about your estimate adjust these values.
+      </p>
+      <p>
+        The number of months forecasted can be customised, and defaults to 36.
+      </p>
     </div>
   );
 };
