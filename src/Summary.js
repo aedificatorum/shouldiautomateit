82dd1f13aef6024e5d2470c5e2a57b864f2b787c
celinechole <github@celinechole.com>
2019-06-08 17:00:54 -0400

    update summary layout

--- src/Summary.js
+++ src/Summary.js
@@ -3,34 +3,32 @@ import React from "react";
 const Summary = ({ calculationsTable }) => {
   console.log(calculationsTable[1]);
   return (
-    <div className="row">
-      <div className="col summary">
-      <p>Summary</p>
-      <p>
+    <div className="row summary">
+      <div className="col">Summary</div>
+      <div className="col">
         {calculationsTable[2]
           ? "3 months: " + calculationsTable[2].shouldIAutomate
           : ""}
-      </p>
-      <p>
+      </div>
+      <div className="col">
         {calculationsTable[5]
           ? "6 months: " + calculationsTable[5].shouldIAutomate
           : ""}
-      </p>
-      <p>
+      </div>
+      <div className="col">
         {calculationsTable[11]
           ? "12 months: " + calculationsTable[11].shouldIAutomate
           : ""}
-      </p>
-      <p>
+      </div>
+      <div className="col">
         {calculationsTable[23]
           ? "24 months: " + calculationsTable[23].shouldIAutomate
           : ""}
-      </p>
-      <p>
+      </div>
+      <div className="col">
         {calculationsTable[35]
           ? "36 months: " + calculationsTable[35].shouldIAutomate
           : ""}
-      </p>
       </div>
     </div>
   );
